941842372218d9136c4635f4f40130965d7bb994
“ishtiaqniloy” <1505080.aai@ugrad.cse.buet.ac.bd>
2018-04-26 21:27:57 +0600

    Burger Class created and finalized

--- src/Scenerio1/Burger.java
+++ src/Scenerio1/Burger.java
@@ -3,9 +3,24 @@ package Scenerio1;
 import java.util.ArrayList;
 
 public class Burger {
-    private ArrayList <String> components = new ArrayList<String>();
+    private ArrayList <String> components;
 
     Burger(){
+        components = new ArrayList<String>();
+    }
+
+    boolean add(String str){
+        components.add(str);
+
+        return true;
+
+    }
+
+    void show(){
+        System.out.println("Components of this burger:");
+        for(String str: components){
+            System.out.println(str);
+        }
 
     }
 
