be93b3b591d26ec12bd0f546e9ee174287d22942
“ishtiaqniloy” <1505080.aai@ugrad.cse.buet.ac.bd>
2018-04-26 21:25:00 +0600

    BurgerBuilder Interface created

---
+++ src/Scenerio1/Burger.java
@@ -0,0 +1,14 @@
+package Scenerio1;
+
+import java.util.ArrayList;
+
+public class Burger {
+    private ArrayList <String> components = new ArrayList<String>();
+
+    Burger(){
+
+    }
+
+
+
+}
---
+++ src/Scenerio1/Waiter.java
@@ -0,0 +1,4 @@
+package Scenerio1;
+
+public class Waiter {
+}
