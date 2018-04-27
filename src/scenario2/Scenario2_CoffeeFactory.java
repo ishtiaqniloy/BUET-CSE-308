66c87cd1588160cb19fc96f035df3e631c9f1f7c
ishtiaqniloy <1505080.aai@ugrad.cse.buet.ac.bd>
2018-04-27 18:49:35 +0600

    Main Function created

---
+++ src/scenario2/Scenario2_CoffeeFactory.java
@@ -0,0 +1,19 @@
+package scenario2;
+public class Scenario2_CoffeeFactory {
+    public static void main(String []args){
+        System.out.println("Coffee Factory");
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+}
