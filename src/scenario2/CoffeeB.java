de5af55c092814aa73daead550ef437170ecb524
ishtiaqniloy <1505080.aai@ugrad.cse.buet.ac.bd>
2018-04-27 19:09:55 +0600

    CoffeeB written

---
+++ src/scenario2/CoffeeB.java
@@ -0,0 +1,11 @@
+package scenario2;
+
+public class CoffeeB implements Coffee{
+    @Override
+    public void addComponents(){
+        System.out.println("Hot water taken");
+        System.out.println("Coffee added");
+        System.out.println("Milk added");
+    }
+
+}
