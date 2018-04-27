d0a8e5483e6e1131ded02b62df3a3e1b77c265d9
ishtiaqniloy <1505080.aai@ugrad.cse.buet.ac.bd>
2018-04-27 19:14:58 +0600

    CoffeeFactory written

--- src/scenario2/CoffeeFactory.java
+++ src/scenario2/CoffeeFactory.java
@@ -1,6 +1,20 @@
 package scenario2;
 
 public class CoffeeFactory {
+    Coffee prepareCoffee(String type){
+        Coffee coffee = null;
 
+        if(type.equalsIgnoreCase("A")){
+            coffee = new CoffeeA();
+        }
+        else if(type.equalsIgnoreCase("B")){
+            coffee = new CoffeeB();
+        }
+        else if(type.equalsIgnoreCase("C")){
+            coffee = new CoffeeC();
+        }
+
+        return coffee;
+    }
 
 }
