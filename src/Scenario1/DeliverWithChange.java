f30fdd0698b7e3507bf8bcd8ca12ca3dc1d1039b
ishtiaqniloy <1505080.aai@ugrad.cse.buet.ac.bd>
2018-06-20 01:57:17 +0600

    DeliverWithChange written

--- src/Scenario1/DeliverWithChange.java
+++ src/Scenario1/DeliverWithChange.java
@@ -1,17 +1,16 @@
 package Scenario1;
 
-public class DeliverNoChange implements State {
-
+public class DeliverWithChange implements State {
     @Override
     public void doAction(Context context) {
         context.vendingMachine.deliverItem(context.productName);
-        System.out.println("No change is given");
+        System.out.println("Change = " + (context.vendingMachine.getProductInfo(context.productName).price-context.coinInserted) );
 
         context.setState(this);
     }
 
     @Override
     public String toString() {
-        return "DeliverNoChange State";
+        return "DeliverWithChange State";
     }
 }
